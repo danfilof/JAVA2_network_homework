61562c6d075e40f60e4559b87dced7c0fb9b0064
danfilof <danila.anfilofyev@gmail.com>
2022-01-25 22:21:25 +0100

    Comments added

--- src/main/java/ru/gb/HW_Client.java
+++ src/main/java/ru/gb/HW_Client.java
@@ -17,17 +17,19 @@ public class HW_Client {
     }
 
     public HW_Client() {
-       start();
+       receive();
        send();
     }
 
     private void send() {
         Scanner scanner1 = new Scanner(System.in);
+        // В бесконечном цикле, отсылать все, что получил scanner1. В случае закрытия сокета, цикл прекращается
         while (true) {
             try {
                 if (socket.isClosed()){
                     System.out.println("Socket is closed");
                     break;
+                    // При нажатии на enter или попытке что-либо послать, срабатывает вышеуказанный код, так как сокет уже закрыт, цикл прекращается, exit code 0
                 }
                 out.writeUTF(scanner1.nextLine());
             } catch (IOException e) {
@@ -36,22 +38,24 @@ public class HW_Client {
         }
     }
 
-    private void start() {
+    private void receive() {
         try {
             socket = new Socket("localhost",8089);
             in = new DataInputStream(socket.getInputStream());
             out =  new DataOutputStream(socket.getOutputStream());
+            // Запускаю поток, считывающий все, приходящее на порт
             new Thread(() ->  {
                 try {
+                    // В бесконечном цикле, получаю все сообщения, приходящие на порт. Если оно значит "/end", отсылаю обратно то же сообщение и прерываю цикл после подтверждения в консоли
                     while (true) {
                         final String message = in.readUTF();
-                        System.out.println("Server: " + message);
                         if ("/end".equalsIgnoreCase(message)) {
                             out.writeUTF("/end");
-                            System.out.println("Received command: end");
+                            System.out.println("Received command: END");
                             closeConnection();
                             break;
                         }
+                        System.out.println("Server: " + message);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -61,7 +65,7 @@ public class HW_Client {
             e.printStackTrace();
         }
     }
-
+ // При одном из трех сценариев сокет закрывается. Если не срабатывает одно условие или дает исключение, ошибка обрабатывается и метод работает дальше
     private void closeConnection() {
         if (in != null) {
             try {
--- src/main/java/ru/gb/HW_Server.java
+++ src/main/java/ru/gb/HW_Server.java
@@ -17,7 +17,7 @@ public class HW_Server {
     }
 
     public HW_Server() {
-       start();
+       receive();
         send();
         }
 
@@ -25,16 +25,19 @@ public class HW_Server {
             String s;
             Scanner scanner2 = new Scanner(System.in);
             s = scanner2.nextLine();
+            // По какой-то причине пришлось делать такой небольшой костыль, иначе IDE выдвала nullpointerexception на первое введенное из scanner2 значение
             try {
                 out.writeUTF(s);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            // В бесконечном цикле, отсылать все, что получил scanner2. В случае закрытия сокета, цикл прекращается
             while (true) {
                 try {
                     if (socket.isClosed()) {
                         System.out.println("Socket is closed");
                         break;
+                        // При нажатии на enter или попытке что-либо послать, срабатывает вышеуказанный код, так как сокет уже закрыт, цикл прекращается, exit code 0
                     }
                     out.writeUTF(scanner2.nextLine());
                 } catch (Exception e) {
@@ -43,17 +46,21 @@ public class HW_Server {
             }
         }
 
-    private void start() {
+    private void receive() {
          socket = null;
+         // Запускаю поток, считывающий все, приходящее на порт
         new Thread(() -> {
             try (ServerSocket serverSocket = new ServerSocket(8089)) {
                 System.out.println("Server is on, waiting for connection...");
                 socket = serverSocket.accept();
+                // Добавил возможность увидеть имя клиента, который присоединился
                 String hostName = socket.getInetAddress().getHostName();
                 System.out.printf("The client %s has connected.", hostName);
                 System.out.println("\n");
                 in = new DataInputStream(socket.getInputStream());
                 out = new DataOutputStream(socket.getOutputStream());
+
+                // В бесконечном цикле, получаю все сообщения, приходящие на порт. Если оно значит "/end", отсылаю обратно то же сообщение и прерываю цикл после подтверждения в консоли
                 while (true) {
                     final String message = in.readUTF();
                     if ("/end".equalsIgnoreCase(message)) {
@@ -66,6 +73,7 @@ public class HW_Server {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            // Если на сокете ничего нет, закрываю его
             if (socket != null) {
                 try {
                     socket.close();
